afc5addcb37456625403d9b9a4b2e66c345621e3
priyesh-shah <priyesh.0504@gmail.com>
2021-12-09 17:52:27 +0530

    model2 is final model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("adding all ML related code here")
+
+def model1():
+    print("adding code for Model1")
+
+def model2():
+    print("adding code for Model2")
+
+def model3():
+    print("adding code for Model3")
+
+print("mddel2 is performing best")
